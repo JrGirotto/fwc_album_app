a11552133f31d5c686c94087ef85f016686438c5
Junior Girotto <jrgirotto@live.com>
2024-10-16 16:38:35 -0300

    initial commit

---
+++ android/app/src/main/kotlin/br/com/jrgirotto/fwc_album_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.jrgirotto.fwc_album_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/fwc_album_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:fwc_album_app/app/pages/splash/splash_page.dart';
+
+class FwcAlbumApp extends StatelessWidget {
+  const FwcAlbumApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fifa World Cup Album',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => const SplashPage(),
+      },
+    );
+  }
+}

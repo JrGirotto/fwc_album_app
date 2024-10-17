9b51cbd27bc85b53984afb29f4ed022ebc195fcd
Junior Girotto <jrgirotto@live.com>
2024-10-17 18:27:59 -0300

    Repository / Exception

--- lib/app/fwc_album_app.dart
+++ lib/app/fwc_album_app.dart
@@ -3,8 +3,9 @@ import 'package:flutter_getit/flutter_getit.dart';
 import 'package:fwc_album_app/app/core/rest/custom_dio.dart';
 import 'package:fwc_album_app/app/pages/auth/login/login_page.dart';
 import 'package:fwc_album_app/app/pages/home/home_page.dart';
-import 'package:fwc_album_app/app/pages/splash/splash_page.dart';
 import 'package:fwc_album_app/app/pages/splash/view/splash_route.dart';
+import 'package:fwc_album_app/app/repository/auth/auth_repository.dart';
+import 'package:fwc_album_app/app/repository/auth/auth_repository_impl.dart';
 
 class FwcAlbumApp extends StatelessWidget {
   const FwcAlbumApp({super.key});
@@ -14,8 +15,9 @@ class FwcAlbumApp extends StatelessWidget {
     return FlutterGetItApplicationBinding(
       bindingsBuilder: () => [
         Bind.lazySingleton<CustomDio>(
-          (i) => CustomDio(),
-        )
+          (i) => CustomDio()),
+        Bind.lazySingleton<AuthRepository>((i) => AuthRepositoryImpl(dio: i())),
+        
       ],
       child: MaterialApp(
         title: 'Fifa World Cup Album',

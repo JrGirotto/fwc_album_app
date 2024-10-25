0dd269d6ed34e56faa56c4005eb992298a56d048
Junior Girotto <jrgirotto@live.com>
2024-10-25 17:38:02 -0300

    Buscar Figurinhas (Tela Detalhes)

--- lib/app/repository/stickers/stickers_repository_impl.dart
+++ lib/app/repository/stickers/stickers_repository_impl.dart
@@ -5,6 +5,7 @@ import 'package:dio/dio.dart';
 import 'package:fwc_album_app/app/core/exceptions/repository_exception.dart';
 import 'package:fwc_album_app/app/core/rest/custom_dio.dart';
 import 'package:fwc_album_app/app/models/groups_stickers.dart';
+import 'package:fwc_album_app/app/models/sticker_model.dart';
 import 'package:fwc_album_app/app/repository/stickers/stickers_repository.dart';
 
 class StickersRepositoryImpl implements StickersRepository {
@@ -26,4 +27,28 @@ class StickersRepositoryImpl implements StickersRepository {
       throw RepositoryException(message: 'Erro ao buscar album do usuário');
     }
   }
+
+  @override
+  Future<StickerModel?> findStickerByCode(
+      String stickerCode, String stickerNumber) async {
+    try {
+      final result = await dio.auth().get(
+        '/api/stickers-search',
+        queryParameters: {
+          'sticker_code': stickerCode,
+          'sticker_number': stickerNumber,
+        },
+      );
+      return StickerModel.fromMap(result.data);
+    } on DioError catch (e, s) {
+      if (e.response?.statusCode == 404) {
+        return null;
+      }
+      log('Erro ao buscar figurinha', error: e, stackTrace: s);
+      throw RepositoryException(message: 'Erro ao buscar figurinha');
+    }
+    return null;
+  }
+
+  
 }
